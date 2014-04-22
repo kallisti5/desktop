70e62adae1140562574f0f6af5704532f04c30ce
Alexander von Gluck IV <kallisti5@unixzen.com>
2014-04-22 09:09:09 -0500

    Deploy: drop libconfig as it doesn't exist in repos on Debian

--- deploy.d/00-install-dpkg.sh
+++ deploy.d/00-install-dpkg.sh
@@ -7,7 +7,7 @@ fi
 echo "Installing Debian packages..."
 
 sudo apt-get update
-sudo apt-get install subtle git rxvt-unicode feh ncmpcpp libconfig asciidoc mpd ruby
+sudo apt-get install subtle git rxvt-unicode feh ncmpcpp asciidoc mpd ruby
 gem install archive-tar-minitar
 
 sur install clock2
